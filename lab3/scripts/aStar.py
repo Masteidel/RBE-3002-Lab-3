7b2e373e956b972a2abca5f9f1617576ba5e6115
bheavey <bheavey95@gmail.com>
2017-04-06 00:22:32 -0400

    Update aStar.py

--- lab3/scripts/aStar.py
+++ lab3/scripts/aStar.py
@@ -428,8 +428,8 @@ def get2DArray(data, width, height): #an absolutely thrilling function to take a
         j = 0 #reset index (start at the start of the new row)
 
         while (j < width) and (k < len(data)): #go through a single row
-            #grid[j][i] = cell(data[k], j, i)#creates a cell object
-            grid[j][i]=cell()
+            grid[j][i] = cell(data[k], j, i)#creates a cell object
+            #grid[j][i]=cell()
             grid[j][i].x = j
             grid[j][i].y = i
             grid[j][i].prob = data[k]
